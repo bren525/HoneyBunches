07fdf88ffe54f0777e51f44c7c4a382d2e7185ae
Brendan <brendan@BrenCompRobo.(none)>
2015-05-10 16:17:33 -0400

    working on out of control

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,5 +1,5 @@
 //List of all possible games
-var games = ['SprayTheMost','BigButton'];
+var games = [/*'SprayTheMost','BigButton',*/'OutOfControl'];
 
 var $canvas = $('#demoCanvas');
 var $canvasContainer= $('#canvasContainer')
