c7e02845f51cfb95348d1095b95ddcee312f0902
Chelsea <chelsea.bailey@students.olin.edu>
2015-05-09 22:18:05 -0700

    Button Game!

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,5 +1,5 @@
 //List of all possible games
-var games = ['SprayTheMost'];
+var games = ['SprayTheMost','BigButton'];
 
 var $canvas = $('#demoCanvas');
 var $canvasContainer= $('#canvasContainer')
