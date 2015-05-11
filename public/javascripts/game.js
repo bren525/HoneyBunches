85f0029f5f41249fd4230cd3d74d7d05b661b2d6
Chelsea <chelsea.bailey@students.olin.edu>
2015-05-10 17:48:38 -0700

    Typing Game added!

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,5 +1,5 @@
 //List of all possible games
-var games = ['BigButton','SprayTheMost','MessedUp'];
+var games = ['FastTyper'];
 
 var $canvas = $('#demoCanvas');
 var $canvasContainer= $('#canvasContainer')
